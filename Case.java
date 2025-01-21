370aa1cac959edaa7e209bd7a1f6fc0e009a8f0a
Ethan <ethan.manzi@ynov.com>
2025-01-22 00:30:40 +0100

    add Case

---
+++ Case.java
@@ -0,0 +1,46 @@
+//@SuppressWarnings("ALL")
+public class Case {
+    public boolean blanc;
+    public Pion pion;
+
+    public Case() {
+        this.blanc = false;
+    }
+
+    public void setBlanc() {
+        this.blanc = true;
+    }
+
+    public boolean isBlanc() {
+        return blanc;
+    }
+
+    public boolean isNoir() { return !blanc;}
+
+    public void setPion(Pion pion) {
+        this.pion = pion;
+    }
+
+    public boolean hasPion() {
+        try {
+            //noinspection ResultOfMethodCallIgnored,ResultOfMethodCallIgnored
+            pion.getTypePion();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String toString() {
+        try {
+            //noinspection ResultOfMethodCallIgnored
+            this.pion.getX();
+            return this.pion.toString();
+        } catch (Exception e) {
+            if (this.blanc)
+                return "░";
+            else
+                return " ";
+        }
+    }
+}
