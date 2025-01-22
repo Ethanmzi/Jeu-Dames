56ebc16fdcf6404f17b260307962685172f3ae32
Ethan <ethan.manzi@ynov.com>
2025-01-22 01:19:00 +0100

    Jeu + Plateau

---
+++ Jeu.java
@@ -0,0 +1,35 @@
+public class Jeu {
+    private Plateau plateau;
+    private Joueur joueur1;
+    private Joueur joueur2;
+    private Joueur joueurActuel;
+
+    public Jeu() {
+        initialiser();
+    }
+
+    private void initialiser() {
+        plateau = new Plateau();
+        joueur1 = new Joueur("Joueur 1", true); // Couleur blanche
+        joueur2 = new Joueur("Joueur 2", false); // Couleur noire
+        joueurActuel = joueur1;
+    }
+
+    public void lancer() {
+        InterfaceGraphique interfaceGraphique = new InterfaceGraphique(this, plateau);
+        interfaceGraphique.afficher();
+    }
+
+    public void changerJoueur() {
+        joueurActuel = (joueurActuel == joueur1) ? joueur2 : joueur1;
+    }
+
+    public Joueur getJoueurActuel() {
+        return joueurActuel;
+    }
+
+    public static void main(String[] args) {
+        Jeu jeu = new Jeu();
+        jeu.lancer();
+    }
+}
--- Plateau.java
+++ Plateau.java
@@ -1,120 +1,25 @@
-class Plateau {
+public class Plateau {
     private Case[][] cases;
-    public int taille;
 
-    public Plateau(int taille) {
-        this.taille = taille;
-        this.cases = new Case[taille][taille];
-        boolean doitEtreBlanc;
-        for (int y = 0; y < this.cases.length; y++) {
-            doitEtreBlanc = (y%2 == 0);
-            for (int x = 0; x < this.cases[y].length; x++) {
-                if (doitEtreBlanc) {
-                    this.cases[y][x] = new Case();
-                    this.cases[y][x].setBlanc();
-                    doitEtreBlanc = false;
-                } else {
-                    this.cases[y][x] = new Case();
-                    doitEtreBlanc = true;
+    public Plateau() {
+        cases = new Case[8][8];
+        initialiserPlateau();
+    }
+
+    private void initialiserPlateau() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                cases[i][j] = new Case(i, j);
+                if ((i + j) % 2 == 1) { // Cases noires
+                    if (i < 3) cases[i][j].setPiece(new Pion(false)); // Joueur 2
+                    else if (i > 4) cases[i][j].setPiece(new Pion(true)); // Joueur 1
                 }
             }
         }
     }
 
-    public void afficherPlateau(int[] posCurseur) {
-        //System.out.println("-----------------------------------------");
-        System.out.println("┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐");
-        for (int y=0;y<this.cases.length; y++) {
-            for(int x=0; x<this.cases[y].length; x++) {
-                Case cetteCase = this.cases[y][x];
-                if(x == posCurseur[0] && y == posCurseur[1]) { //le curseur est sur la case
-                    System.out.print("│["+cetteCase+"]");
-                }
-                else{
-                    if(cetteCase.isBlanc())
-                        //System.out.print("│"+x+cetteCase+y+"");
-                        System.out.print("│░"+cetteCase+"░");
-                    else
-                        System.out.print("│   ");
-
-                }
-            }
-            System.out.println("│");
-            if(y!=this.cases.length-1)
-                System.out.println("├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤");
-            else
-                System.out.println("└───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘");
-        }
-    }
-
-    public void afficher(Pion[] pions) {
-        this.update(pions);
-        afficherPlateau(new int[]{-1, -1}); //petit hack pour ne pas avoir à copier/coller du code
-    }
-
-    public Case[][] getPlateau() {
-        return this.cases;
-    }
-
-    public void update(Pion[] pions) {
-        for (Case[] ligne: this.cases) {
-            for(Case cetteCase: ligne)
-                cetteCase.pion = null;
-        }
-        for (int i = 0; i < pions.length; i++) {
-            if (pions[i].isVivant()) // on ne rajoute pas les pions morts à l'affichage
-                this.cases[pions[i].getY()][pions[i].getX()].setPion(pions[i]);
-        }
-    }
-
-    public Pion getPionDepuisCase(int[] pos) { //ça ne fera pas recommencer en cas d'erreur
-
-        try {
-            return this.cases[pos[1]][pos[0]].pion;
-        } catch (Exception e) {
-            //System.out.println("Veuillez séléctionner un pion correct");
-            //e.printStackTrace();
-            return null;
-        }
-    }
-
-    public boolean peutIlManger(Pion pion) {
-        boolean returned = false;
-        for(int x=pion.getX()-2;x<pion.getX()+2;x+=1){
-            for(int y=pion.getY()-2;y<pion.getY()+2;y+=1){
-                int[] curPos = {x, y};
-                //System.out.println("test en x="+x+" y="+y+ " ");
-                if(this.hasPion(curPos)) { //System.out.print("pas vide ");
-                    Pion cibleTest = this.getPionDepuisCase(curPos);
-                    if(!pion.equals(cibleTest)) { //si pion adverse
-                        //System.out.print("cible: "+cibleTest);
-                        String oldTypeCible = cibleTest.getTypePion();
-                        int[] oldPosPion = pion.getPos();
-                        if(pion.mange(cibleTest, this)){ //si on peut le manger
-                            //System.out.print("noice");
-                            returned = true;
-                            cibleTest.setTypePion(oldTypeCible);//on resscucite la cible
-                            pion.setPos(oldPosPion);        //on replace le pion
-
-                        }
-                    }
-                }
-                //else System.out.print("nope");
-            }
-
-        }
-        return returned;
-    }
-
-    public boolean hasPion(int[] pos) {
-        try {
-            return this.cases[pos[1]][pos[0]].hasPion();
-        } catch (Exception e){
-            return false;
-        }
-    }
-
-    public boolean estVide(int[] pos) {
-        return !hasPion(pos);
+    public Case getCase(int x, int y) {
+        if (x >= 0 && x < 8 && y >= 0 && y < 8) return cases[x][y];
+        return null;
     }
 }
